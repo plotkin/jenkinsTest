c41485b7ad94470882356b574b27d578a8ba8011
dgridnev <dmitry.gridnev@appintheair.mobi>
2020-03-12 19:44:42 +0300

    init

---
+++ jenkinsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jenkinsTest
+//
+//  Created by Дмитрий Гриднев on 12.03.2020.
+//  Copyright © 2020 Dmitry Gridnev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Helloq1sad, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
